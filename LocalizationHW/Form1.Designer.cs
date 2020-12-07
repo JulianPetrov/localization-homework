606a3821fe15577778b8a0073c151fb5300d2179
JulianPetrov <j.petrov@blubito.com>
2020-12-07 13:20:42 +0200

    Commit homework project

---
+++ LocalizationHW/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace LocalizationHW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtnChangeToBG = new System.Windows.Forms.RadioButton();
+            this.radioBtnChangeToUS = new System.Windows.Forms.RadioButton();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabPage1
+            // 
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.radioBtnChangeToBG);
+            this.groupBox1.Controls.Add(this.radioBtnChangeToUS);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioBtnChangeToBG
+            // 
+            resources.ApplyResources(this.radioBtnChangeToBG, "radioBtnChangeToBG");
+            this.radioBtnChangeToBG.Name = "radioBtnChangeToBG";
+            this.radioBtnChangeToBG.TabStop = true;
+            this.radioBtnChangeToBG.UseVisualStyleBackColor = true;
+            this.radioBtnChangeToBG.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioBtnChangeToUS
+            // 
+            resources.ApplyResources(this.radioBtnChangeToUS, "radioBtnChangeToUS");
+            this.radioBtnChangeToUS.Name = "radioBtnChangeToUS";
+            this.radioBtnChangeToUS.TabStop = true;
+            this.radioBtnChangeToUS.UseVisualStyleBackColor = true;
+            this.radioBtnChangeToUS.CheckedChanged += new System.EventHandler(this.radioBtnChangeToUS_CheckedChanged);
+            // 
+            // tabPage2
+            // 
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.Name = "btnExit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtnChangeToBG;
+        private System.Windows.Forms.RadioButton radioBtnChangeToUS;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ LocalizationHW/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace LocalizationHW
+{
+    public partial class Form1 : Form
+    {
+
+        string lang;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            lang = "bg-BG";
+            ChangeLanguage(lang);
+        }
+
+        private void radioBtnChangeToUS_CheckedChanged(object sender, EventArgs e)
+        {
+            lang = "en-US";
+            ChangeLanguage(lang);
+        }
+
+        private void ChangeLanguage(string lang)
+        {
+            CultureInfo cultureInfo = new CultureInfo(lang);
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+            ComponentResourceManager resource = new ComponentResourceManager(this.GetType());
+            this.Text = resource.GetString("$this.Text", cultureInfo);
+            ApplyResourcesToControls(this,resource,cultureInfo);
+            
+        }
+
+        private void ApplyResourcesToControls(Control control, ComponentResourceManager resource, CultureInfo cultureInfo)
+        {
+            foreach (Control c in control.Controls)
+            {
+                resource.ApplyResources(c, c.Name, cultureInfo);
+                if(c.HasChildren) 
+                    ApplyResourcesToControls(c,resource,cultureInfo);
+            }
+        }
+
+
+    }
+}
